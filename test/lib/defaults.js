8c72c7c20a41cba4a13260079bc625caca489f55
John Granström <granstrom.john@gmail.com>
2016-05-02 16:27:50 +0200

    Initial tests

---
+++ test/lib/defaults.js
@@ -0,0 +1,17 @@
+const DEFAULTS = require('../../lib/defaults');
+
+describe('defaults', () => {
+  describe('DEFAULT_KEY_FN', () => {
+    it('should return appropriate default key', () => {
+      const key = DEFAULTS.DEFAULT_KEY_FN({ originalUrl: 'abcdef' }, { name: '123' });
+      expect(key).to.equal('123:abcdef');
+    })
+  });
+
+  describe('DEFAULT_TTL_FN', () => {
+    it('should return the default TTL seconds', () => {
+      const ttl = DEFAULTS.DEFAULT_TTL_FN();
+      expect(ttl).to.equal(5);
+    });
+  })
+});
